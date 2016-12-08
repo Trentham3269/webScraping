8406f66f16721bb90e9500d29b3cb326538bc4cd
scottdalwood <scottdalwood@gmail.com>
2016-12-08 15:29:23 +1100

    Incorporate lats and longs into wikipedia data

--- showsScrape.R
+++ showsScrape.R
@@ -1,6 +1,10 @@
 library(rvest)
 library(readr)
 
+# set working directory
+wd <- "~/Documents/Repos/webScraping/"
+setwd(wd)
+
 # current score location
 url <- "https://en.wikipedia.org/wiki/The_River_Tour_2016"
 
@@ -21,10 +25,16 @@ revenue    <- as.data.frame(shows_scrape[[1]][[7]])
 # combine into data frame
 shows <- cbind(date, city, country, venue, attendance, revenue)
 
-# rename columns
-colnames(shows) <- c("Date", "City", "Country", "Venue", "Attendance", "Revenue")
-
 # clean data frame
 excl <- c("North America[27]", "Europe[28]", "North America", "Oceania[3]", "TOTAL")
-shows <- shows[!(shows$Date %in% excl), ]
+shows <- shows[!(shows[,1] %in% excl), ]
+
+# combine with manual lat/longs
+stadiums <- read_csv("riverTour.csv")
+lat  <- as.data.frame(stadiums$Latitude)
+long <- as.data.frame(stadiums$Longitude)
+shows <- cbind(shows, lat, long)
+
+# rename columns
+colnames(shows) <- c("Date", "City", "Country", "Venue", "Attendance", "Revenue", "Latitude", "Longitude")
 
